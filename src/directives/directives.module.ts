3fe0e9fff04c43400344a1fbcdbcdc439742f287
javecruz <clony90@gmail.com>
2018-04-13 01:39:23 +0200

    first

---
+++ src/directives/directives.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MyDirective } from './my/my';
+
+@NgModule({
+	declarations: [MyDirective],
+	imports: [],
+	exports: [MyDirective]
+})
+export class DirectivesModule {}
